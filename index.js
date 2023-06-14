8be8c6655328871140d337eccb645f04a9893c1d
Joy Olago <joydanielle62@gmail.com>
2023-06-14 12:50:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Joy O'
+
+let welcomeMessage = 'Welcome to Flatbook, Joy O!'
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, JOY O!'
+
+
+let shortGreeting = 'Welcome, J!'
+const firstInitial = currentUser (J) ;
+const restOfName = currentUser (oy)
